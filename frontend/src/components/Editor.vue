8bb77cb5f15f77695e5b43975c72235885264d24
chenguixin <guixinchan@qq.com>
2022-04-10 23:40:30 +0800

    add codemirror dependency

--- frontend/src/components/Editor.vue
+++ frontend/src/components/Editor.vue
@@ -1,9 +1,36 @@
-<template>
-</template>
-
 <script setup>
+import { EditorState } from '@codemirror/state'
+import { EditorView, keymap } from '@codemirror/view'
+import { defaultKeymap } from '@codemirror/commands'
+import { lineNumbers } from '@codemirror/gutter'
+
+let startState = EditorState.create({
+  doc: 'Hello World',
+  extensions: [keymap.of(defaultKeymap), lineNumbers()],
+})
+EditorView.baseTheme({
+  '.cm-gutters': {
+    backgroundColor: '#2c3e50',
+      color: '#2c3e50'
+  },
+})
+
+const view = ref({})
+onMounted(() => {
+  console.log(document.querySelector('#editor'))
+  view.value = new EditorView({
+    state: startState,
+    parent: document.querySelector('#editor'),
+  })
+})
 </script>
 
-<style scoped>
+<template>
+  <div id="editor" class="text-left border m-2" />
+</template>
 
+<style>
+.dark-gutter {
+  background: #2c3e50;
+}
 </style>
