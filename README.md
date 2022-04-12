c2f52ad72ab4ee6d4d845b8bb03b3fbbb9e741ed
chenguixin <591764955@qq.com>
2022-04-12 09:19:32 +0800

    add CodeMirror component and random EXP generator

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Packages
 
 - [GitHub Corners](https://github.com/tholman/github-corners)
+- [codemirror.next](https://github.com/codemirror/codemirror.next)
+- [regulex](https://github.com/CJex/regulex)
+- [regulex_common](https://github.com/wyc194/regulex)
+- [randexp](https://github.com/fent/randexp.js)
--- frontend/src/components/Editor.vue
+++ frontend/src/components/Editor.vue
@@ -1,36 +1,132 @@
 <script setup>
-import { EditorState } from '@codemirror/state'
-import { EditorView, keymap } from '@codemirror/view'
-import { defaultKeymap } from '@codemirror/commands'
-import { lineNumbers } from '@codemirror/gutter'
+import CodeMirrorEditor from './CodeMirrorEditor.vue'
+import { SIMPLE_CASES_QUERY } from '../types/query.ts'
+import { parse, Kit, Raphael, visualize } from 'regulex_common'
+import FlagIcon from './icons/FlagIcon.vue'
+import RandExp from 'randexp'
 
-let startState = EditorState.create({
-  doc: 'Hello World',
-  extensions: [keymap.of(defaultKeymap), lineNumbers()],
-})
-EditorView.baseTheme({
-  '.cm-gutters': {
-    backgroundColor: '#2c3e50',
-      color: '#2c3e50'
+const { result } = useQuery(SIMPLE_CASES_QUERY)
+const caseList = useResult(result, [])
+
+const regex = ref('')
+const flags = ref([])
+const flagOptions = [
+  {
+    label: 'ignore case (i)',
+    value: 'i',
   },
+  {
+    label: 'global (g)',
+    value: 'g',
+  },
+  {
+    label: 'multiline (m)',
+    value: 'm',
+  },
+]
+
+let paper = null
+
+onMounted(() => {
+  paper = Raphael('regex-image', 0, 0)
+})
+/**
+ * 可视化 regex expression
+ */
+function visualizeRegex() {
+  if (isValidRegex.value) {
+    const regExp = new RegExp(regex.value, viewFlags.value)
+    try {
+      visualize(parse(regExp.source, false), getRegexFlags(regExp), paper)
+    } catch (e) {
+      if (e instanceof parse.RegexSyntaxError) {
+        alert('generate failed')
+      }
+    }
+  }
+}
+
+function getRegexFlags(re) {
+  let flags = ''
+  flags += re.ignoreCase ? 'i' : ''
+  flags += re.global ? 'g' : ''
+  flags += re.multiline ? 'm' : ''
+  return flags
+}
+
+function changeHeightLight() {
+  visualizeRegex()
+}
+
+const viewFlags = computed(() => flags.value.join(''))
+
+/**
+ * regex expression 是否合法
+ * @type {ComputedRef<boolean>}
+ */
+const isValidRegex = computed(() => {
+  if (!regex.value || regex.value.length === 0) {
+    return false
+  }
+
+  try {
+    new RegExp(regex.value, viewFlags.value)
+  } catch (e) {
+    return false
+  }
+  return true
 })
 
-const view = ref({})
-onMounted(() => {
-  console.log(document.querySelector('#editor'))
-  view.value = new EditorView({
-    state: startState,
-    parent: document.querySelector('#editor'),
-  })
-})
+const generateRandomExp = () => RandExp.randexp(regex)
 </script>
 
 <template>
-  <div id="editor" class="text-left border m-2" />
+  <n-grid cols="1 800:3" x-gap="16" y-gap="16" item-responsive>
+    <n-gi span="1 800:2">
+      <n-space vertical>
+        <n-input placeholder="请输入正则表达式" class="text-left" @input="changeHeightLight" maxlength="1000" v-model:value="regex">
+          <template #prefix>/</template>
+          <template #suffix>
+            <span>/</span>
+            <n-popselect v-model:value="flags" multiple :options="flagOptions">
+              <div class="flex items-center">
+                <span id="flags" class="mx-1 cursor-pointer">{{ viewFlags }}</span>
+                <n-icon class="cursor-pointer" :component="FlagIcon" />
+              </div>
+            </n-popselect>
+          </template>
+        </n-input>
+
+        <CodeMirrorEditor />
+
+        <div id="regex-image" v-show="isValidRegex"></div>
+      </n-space>
+    </n-gi>
+    <n-gi>
+      <n-space vertical>
+        <n-card v-if="isValidRegex" title="匹配串生成器" class="bg-opacity-50" embedded>
+          {{ RandExp.randexp(regex) }}
+          <template #action>
+            <n-button @click="generateRandomExp" class="w-full text-white" type="success" strong secondary>重新生成</n-button>
+          </template>
+        </n-card>
+
+        <n-list bordered>
+          <template #header>推荐列表</template>
+          <n-list-item v-for="aCase in caseList">
+            <n-thing :title="aCase.title" />
+          </n-list-item>
+        </n-list>
+      </n-space>
+    </n-gi>
+  </n-grid>
 </template>
 
 <style>
-.dark-gutter {
-  background: #2c3e50;
+#regex-image {
+  @apply w-full bg-neutral-200 rounded overflow-auto py-4;
+}
+#regex-image svg {
+  @apply my-0 mx-auto;
 }
 </style>
