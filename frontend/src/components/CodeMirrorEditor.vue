22c5de064cdaf84ceff07ffd90471b7475390ee2
chenguixin <591764955@qq.com>
2022-04-12 09:19:32 +0800

    add CodeMirror component and random EXP generator

---
+++ frontend/src/components/CodeMirrorEditor.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="editor" class="text-left border rounded-md overflow-hidden" />
+</template>
+
+<script setup>
+import { EditorState } from '@codemirror/state'
+import { EditorView, keymap } from '@codemirror/view'
+import { defaultKeymap } from '@codemirror/commands'
+import { lineNumbers } from '@codemirror/gutter'
+
+let startState = EditorState.create({
+  doc: 'Hello World',
+  extensions: [keymap.of(defaultKeymap), lineNumbers()],
+})
+
+const view = ref({})
+onMounted(() => {
+  console.log(document.querySelector('#editor'))
+  view.value = new EditorView({
+    state: startState,
+    parent: document.querySelector('#editor'),
+  })
+})
+</script>
+
+<style>
+</style>
--- frontend/src/types/query.ts
+++ frontend/src/types/query.ts
@@ -9,3 +9,12 @@ export const CASES_QUERY = gql`
     }
   }
 `
+
+export const SIMPLE_CASES_QUERY = gql`
+  query Cases {
+    getCases {
+      _id
+      title
+    }
+  }
+`
