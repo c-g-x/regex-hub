10edd49fa8c28ef1729226bab24ee885d3bb360a
chenguixin <591764955@qq.com>
2022-04-18 22:25:04 +0800

    add docSizePlugin and docChangeUpdaterPlugin

--- frontend/src/components/CodeMirrorEditor.vue
+++ frontend/src/components/CodeMirrorEditor.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="editor" class="text-left border rounded-md overflow-hidden" />
+  <div id="editor" class="editor-container" />
 </template>
 
 <script setup lang="ts">
@@ -7,31 +7,60 @@ import { EditorState } from '@codemirror/state'
 import { EditorView, keymap, ViewPlugin, ViewUpdate } from '@codemirror/view'
 import { defaultKeymap } from '@codemirror/commands'
 import { lineNumbers } from '@codemirror/gutter'
+import { Ref } from 'vue'
 
-let startState = EditorState.create({
-  doc: 'Hello World',
-  extensions: [
-    keymap.of(defaultKeymap),
-    lineNumbers(),
-    ViewPlugin.fromClass(
-      class {
-        constructor(view: EditorView) {}
-
-        update(update: ViewUpdate) {
-          const { docChanged, state } = update
-          if (update.docChanged) {
-            console.log(state)
-          }
-        }
-      }
-    ),
-  ],
+const data: { view?: EditorView } = reactive({
+  view: undefined,
 })
 
-const view = ref({})
-EditorView.theme({}, { dark: true })
+// 暗色主题
+const darkTheme = EditorView.theme({}, { dark: true })
+
+// 显示文档大小插件
+const docSizePlugin = ViewPlugin.fromClass(
+  class {
+    private dom: any
+    constructor(view: EditorView) {
+      this.dom = view.dom.appendChild(document.createElement('div'))
+      this.dom.style.cssText = 'position: absolute; inset-block-end: 4px; inset-inline-end: 5px;'
+      this.dom.textContent = 'total: ' + view.state.doc.length
+    }
+
+    update(update: ViewUpdate) {
+      if (update.docChanged) {
+        this.dom.textContent = 'total: ' + update.state.doc.length
+      }
+    }
+
+    destroy() {
+      this.dom.remove()
+    }
+  }
+)
+
+// 监听 doc 变化的插件
+const domChangeUpdaterPlugin = ViewPlugin.fromClass(
+  class {
+    constructor(view: EditorView) {}
+
+    update(update: ViewUpdate) {
+      const { docChanged, state } = update
+      if (update.docChanged) {
+        console.log(state)
+      }
+    }
+  }
+)
+
+// 创建编辑器初始状态
+const startState: EditorState = EditorState.create({
+  doc: 'Hello World',
+  extensions: [keymap.of(defaultKeymap), lineNumbers(), domChangeUpdaterPlugin, darkTheme, docSizePlugin],
+})
+
+const view: Ref = ref({})
 onMounted(() => {
-  view.value = new EditorView({
+  data.view = new EditorView({
     state: startState,
     parent: document.querySelector('#editor') as Element,
   })
@@ -39,7 +68,8 @@ onMounted(() => {
 </script>
 
 <style>
-.ͼ2 .cm-content {
-  caret-color: rgba(255, 255, 255, 0.8) !important;
+.editor-container {
+  @apply text-left rounded-md overflow-hidden;
+  border: 2px solid rgba(255, 255, 255, 0.2);
 }
 </style>
