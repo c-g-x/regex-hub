c11261ac9458e4c58fa2ae0c0ddb1cb679b6b938
chenguixin <591764955@qq.com>
2022-05-03 17:14:56 +0800

    add register function

--- frontend/src/Frame.vue
+++ frontend/src/Frame.vue
@@ -6,43 +6,64 @@ import GithubCorner from './components/GithubCorner.vue'
 import ProfileIcon from './components/icons/ProfileIcon.vue'
 import AddIcon from './components/icons/AddIcon.vue'
 import StarIcon from './components/icons/StarIcon.vue'
-import { LOGIN } from './types/query'
+import { LOGIN, REGISTER } from './types/query'
 
 const router = useRouter()
 const mode = reactive({
   isLogin: false,
   showLoginModal: false,
   isRegisterMode: false,
-  enabledRequest: false,
 })
 const userParam = reactive({
   username: '',
   password: '',
+  passwordAgain: '',
 })
 let userInfo = ref({})
 
-const { result } = useQuery(LOGIN, userParam, () => ({
-  enabled: mode.enabledRequest,
-}))
-userInfo.value = useResult(result, {})
-console.log(userInfo.value)
+const { result } = useQuery(LOGIN, userParam)
 
 let message = useMessage()
 function login() {
   if (userParam.username.length === 0 || userParam.password.length === 0) {
     return message.info(`用户名或密码不能为空`)
   }
-  mode.enabledRequest = true
 
-  if (result.value) {
-    message.success(`登录成功，${result.loginUser.username}`)
+  userInfo = useResult(result, {})
+
+  if (userInfo.value) {
+    message.success(`登录成功，${userInfo.value.username}`)
     mode.isLogin = true
     mode.showLoginModal = false
   } else {
     message.error(`登录失败，用户名或密码错误`)
   }
-  mode.enabledRequest = false
 }
+
+const { mutate: registerMutate, onDone: onRegistered } = useMutation(REGISTER, () => ({
+  variables: userParam,
+}))
+function register() {
+  if (userParam.username.length === 0 || userParam.password.length === 0) {
+    return message.info(`用户名或密码不能为空`)
+  }
+
+  if (userParam.password !== userParam.passwordAgain) {
+    return message.error(`重复输入密码不一致，请确认`)
+  }
+
+  registerMutate()
+}
+onRegistered(({ data: { registerUser } }) => {
+  if (registerUser) {
+    userInfo.value = registerUser
+    message.success(`登录成功，${userInfo.value.username}`)
+    mode.showLoginModal = false
+    mode.isLogin = true
+  } else {
+    message.error(`注册失败，该用户已存在`)
+  }
+})
 
 function toIndex() {
   router.push('/')
@@ -62,7 +83,9 @@ function toIndex() {
           <n-space class="mr-10">
             <n-button>关于</n-button>
 
-            <n-button v-if="!mode.isLogin" @click="mode.showLoginModal = true">登录</n-button>
+            <n-button v-if="!mode.isLogin" @click=";(mode.showLoginModal = true) && (mode.isRegisterMode = false)">
+              登录
+            </n-button>
             <n-button-group v-else>
               <n-button ghost>
                 <template #icon>
@@ -100,13 +123,29 @@ function toIndex() {
       <n-input v-model:value="userParam.username" placeholder="请输入用户名" />
     </n-form-item>
     <n-form-item label="密码">
-      <n-input v-model:value="userParam.password" placeholder="请输入密码" />
+      <n-input
+        v-model:value="userParam.password"
+        placeholder="请输入密码"
+        type="password"
+        show-password-on="mousedown"
+      />
+    </n-form-item>
+    <n-form-item label="重复密码" v-if="mode.isRegisterMode">
+      <n-input
+        v-model:value="userParam.passwordAgain"
+        placeholder="请再次输入密码"
+        type="password"
+        show-password-on="mousedown"
+      />
     </n-form-item>
 
     <template #footer>
-      <n-space vertical>
+      <n-space vertical v-if="mode.isRegisterMode" key="register">
+        <n-button quaternary type="info" class="w-full" @click="register">注册</n-button>
+      </n-space>
+      <n-space vertical v-else key="login">
         <n-button strong secondary type="primary" class="w-full" @click="login"> 登录 </n-button>
-        <n-button quaternary type="info" class="w-full" @click="register">没有账号？注册</n-button>
+        <n-button quaternary type="info" class="w-full" @click="mode.isRegisterMode = true">没有账号？注册</n-button>
       </n-space>
     </template>
   </n-modal>
